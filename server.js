796e5d65b4e29391b3ef9691756e959fb279a9b9
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-02-25 17:20:33 -0600

    implemented movie call

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const MOVIES = require('./movies-data.json')
 const cors = require('cors')
 const helmet = require('helmet')
+const MOVIES = require('./movies-data.json')
 
 console.log(process.env.API_TOKEN)
 
@@ -22,6 +22,30 @@ app.use(function validateBearerToken(req, res, next) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
     next()
+})
+
+app.get('/movie', function handleGetMovie(req, res) {
+    let response = MOVIES;
+
+    if (req.query.genre) {
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+    }
+
+    if (req.query.country) {
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+
+    if (req.query.avg_vote) {
+        response = response.filter(movie =>
+            Number(movie.avg_vote) >= Number(req.query.avg_vote)
+        )
+    }
+
+    res.json(response)
 })
 
 const PORT = 8000
